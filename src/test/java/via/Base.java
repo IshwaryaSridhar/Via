f462e8a8016882fe16e5c26f9e1af6080ce1cf69
Ishwarya <s.ishwarya@yahoo.com>
2021-10-04 16:24:15 +0530

    Update 7

--- src/test/java/via/Base.java
+++ src/test/java/via/Base.java
@@ -1,34 +1,47 @@
 package via;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-public class Base 
-{
-
-
+public class Base {
 	public WebDriver driver;
-	@BeforeTest 
-	  public void beforeTest() 
-	  {
-		  System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");	   
-		   ChromeOptions options=new ChromeOptions();
-		   options.addArguments("--disable-notifications");
-		   driver=new ChromeDriver(options);
-		   driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-		   driver.manage().window().maximize();
-	  } 
+  @BeforeTest
+  public void beforeTest() throws Exception{
+	  Properties p =new Properties();
+	  p.load(new FileInputStream("settings.property"));
+	  
+	  if(p.getProperty("browser").equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", p.getProperty("cpath"));
+			driver=new ChromeDriver();
+			ChromeOptions options =new ChromeOptions();
+			options.addArguments("--disable-notifications");
+			driver=new ChromeDriver(options);
+		}
+		else if(p.getProperty("browser").equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", p.getProperty("fpath"));
+			driver=new FirefoxDriver();
+		}
+	  
+	  driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+  }
 
-	  @AfterTest  
-	  public void afterTest() throws Exception 
-	  {  Thread.sleep(2000);
-		  driver.quit();
-		 
-	  }
+  @AfterTest
+  public void afterTest() throws Exception {
+	  
+	  Thread.sleep(1000);
+	  driver.quit();
+	  
+  }
 
 }
--- src/test/java/via/SignUp.java
+++ src/test/java/via/SignUp.java
@@ -12,59 +12,87 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 
 public class SignUp extends Base {
 	
-	Excel_Import ex= new Excel_Import("src/test/resources/data/Data.xlsx");
 	Properties p =new Properties();
+	Excel_Import ex= new Excel_Import("src/test/resources/data/Data.xlsx"); //excel data sheet importing
+	ExtentReports ext=new ExtentReports(); 
+	ExtentTest tc;
 
 	public void getData(String email, String name, String pwd, String mobile) throws Exception {
 	  
 	  p.load(new FileInputStream("settings.property"));
+	  ext.attachReporter(new ExtentHtmlReporter("signup.html"));
+	  
 	  driver.get(p.getProperty("url"));
-	  driver.findElement(By.id("SignIn")).click();
-	  driver.findElement(By.xpath("//span[normalize-space()='SIGN UP']")).click();
-	  driver.findElement(By.xpath("//input[@id='emailIdSignUp']")).sendKeys(email);
-	  driver.findElement(By.xpath("//input[@id='nameSignUp']")).sendKeys(name);
-	  driver.findElement(By.xpath("//input[@id='passwordSignUp']")).sendKeys(pwd);
-	  driver.findElement(By.xpath("//input[@id='mobileNoSignUp']")).sendKeys(mobile);
-	  driver.findElement(By.xpath("//input[@id='signUpValidate']")).click(); 
+	  driver.findElement(By.id(p.getProperty("signin"))).click();
+	  driver.findElement(By.xpath(p.getProperty("signup"))).click();
+	  driver.findElement(By.xpath(p.getProperty("email"))).sendKeys(email);
+	  driver.findElement(By.xpath(p.getProperty("name"))).sendKeys(name);
+	  driver.findElement(By.xpath(p.getProperty("pwd"))).sendKeys(pwd);
+	  driver.findElement(By.xpath(p.getProperty("mob"))).sendKeys(mobile);
+	  driver.findElement(By.xpath(p.getProperty("fin"))).click(); 
 	}
 	
   @Test(enabled=true, description="Testing with valid credentials")
   public void tc_13() throws Exception {
+	  
 
 	  getData(ex.getData("Sheet1", 1, 0),ex.getData("Sheet1", 1, 1),ex.getData("Sheet1", 1, 2),ex.getData("Sheet1", 1, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("valid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println(driver.findElement(By.xpath(p.getProperty("valid"))).getText());
+	  tc=ext.createTest("SignupTest1.html");
+	  tc.info("Testing with valid credentials");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with existing email and mobile")
   public void tc_14() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 2, 0),ex.getData("Sheet1", 2, 1),ex.getData("Sheet1", 2, 2),ex.getData("Sheet1", 2, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Email/Mobile already registered with us");
+	  tc=ext.createTest("SignupTest2.html");
+	  tc.info("Testing with existing email and mobile");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with existing email")
   public void tc_15() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 3, 0),ex.getData("Sheet1", 3, 1),ex.getData("Sheet1", 3, 2),ex.getData("Sheet1", 3, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Email/Mobile already registered with us");
+	  tc=ext.createTest("SignupTest3.html");
+	  tc.info("Testing with existing email");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with existing mobile")
   public void tc_16() throws Exception {
+	  
 	 
 	  getData(ex.getData("Sheet1", 4, 0),ex.getData("Sheet1", 4, 1),ex.getData("Sheet1", 4, 2),ex.getData("Sheet1", 4, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Email/Mobile already registered with us");
+	  tc=ext.createTest("SignupTest4.html");
+	  tc.info("Testing with existing mobile");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with invalid email")
@@ -74,79 +102,124 @@ public class SignUp extends Base {
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter Valid Email");
+	  tc=ext.createTest("SignupTest5.html");
+	  tc.info("Testing with invalid email");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with invalid mobile")
   public void tc_18() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 6, 0),ex.getData("Sheet1", 6, 1),ex.getData("Sheet1", 6, 2),ex.getData("Sheet1", 6, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter Valid Mobile");
+	  tc=ext.createTest("SignupTest6.html");
+	  tc.info("Testing with invalid mobile");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with invalid password")
   public void tc_19() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 7, 0),ex.getData("Sheet1", 7, 1),ex.getData("Sheet1", 7, 2),ex.getData("Sheet1", 7, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Password must be atleast 8 characters long");
+	  tc=ext.createTest("SignupTest7.html");
+	  tc.info("Testing with invalid password");
+	  tc.pass("test pass");
+	 
   }
  
   @Test(enabled=true, description="Testing with invalid name")
   public void tc_20() throws Exception {
+	  
 	 
 	  getData(ex.getData("Sheet1", 8, 0),ex.getData("Sheet1", 8, 1),ex.getData("Sheet1", 8, 2),ex.getData("Sheet1", 8, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter Valid Name");
+	  tc=ext.createTest("SignupTest8.html");
+	  tc.info("Testing with invalid name");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing with null credentials")
   public void tc_21() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 9, 0),ex.getData("Sheet1", 9, 1),ex.getData("Sheet1", 9, 2),ex.getData("Sheet1", 9, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter valid email");
+	  tc=ext.createTest("SignupTest9.html");
+	  tc.info("Testing with null credentials");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing without email")
   public void tc_22() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 10, 0),ex.getData("Sheet1", 10, 1),ex.getData("Sheet1", 10, 2),ex.getData("Sheet1", 10, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter valid email");
+	  tc=ext.createTest("SignupTest10.html");
+	  tc.info("Testing without email");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing without name")
   public void tc_23() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 11, 0),ex.getData("Sheet1", 11, 1),ex.getData("Sheet1", 11, 2),ex.getData("Sheet1", 11, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Enter valid name");
+	  tc=ext.createTest("SignupTest11.html");
+	  tc.info("Testing without name");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing without password")
   public void tc_24() throws Exception {
+	  
 	 
 	  getData(ex.getData("Sheet1", 12, 0),ex.getData("Sheet1", 12, 1),ex.getData("Sheet1", 12, 2),ex.getData("Sheet1", 12, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Password must be atleast 8 characters long");
+	  tc=ext.createTest("SignupTest12.html");
+	  tc.info("Testing without password");
+	  tc.pass("test pass");
+	  
   }
   
   @Test(enabled=true, description="Testing without mobile")
   public void tc_25() throws Exception {
 	  
+	  
 	  getData(ex.getData("Sheet1", 13, 0),ex.getData("Sheet1", 13, 1),ex.getData("Sheet1", 13, 2),ex.getData("Sheet1", 13, 3));
 	  boolean data=driver.findElement(By.xpath(p.getProperty("invalid"))).isDisplayed();
 	  Assert.assertTrue(data);
 	  System.out.println("Mobile Required");
+	  tc=ext.createTest("SignupTest13.html");
+	  tc.info("Testing without mobile");
+	  tc.pass("test pass");
+	  ext.flush();
   }
+  
   
   
 }
